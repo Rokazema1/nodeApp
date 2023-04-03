52b861a971aa7f85551d62a7153e1bf9dc5f5905
Rokazema1 <118417637+Rokazema1@users.noreply.github.com>
2023-04-03 11:29:44 +0530

    Add files via upload

---
+++ university.js
@@ -0,0 +1,13 @@
+//var university=
+//{
+    //Nane: "LPU"
+   // Address: "Phagwara",
+   // Contact: "";
+   // Email: 
+//};
+
+//var x = "Welcome to Lovely Professional University";
+//var y = 'Node.js Tutorials';
+//var z = ['Lovely Professional','University';
+//console]
+
